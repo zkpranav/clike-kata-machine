e0c22e141966e4e3a3610a3611e1d88e4a20476b
zkpranav <zkpranav@icloud.com>
2023-06-06 17:29:24 +0530

    Adding directories for algorithm types; Implemented bubble sort

---
+++ c/sort/bubbleSort.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void bubbleSort(int* haystack, int length);
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
+
+void bubbleSort(int* haystack, int length) {
+    if (length == 0) {
+        return;
+    }
+
+    for (size_t i = 0; i < length; i++) {
+        for (size_t j = 0; j < length - 1 - i; j++) {
+            if (haystack[j] > haystack[j + 1]) {
+                int tmp = haystack[j];
+                haystack[j] = haystack[j + 1];
+                haystack[j + 1] = tmp;
+            }
+        }
+    }
+}
